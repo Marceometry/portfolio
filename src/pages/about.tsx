ff59e96a46a6b7cc361a1254e98101f83153739e
Marcelino <marceometry@gmail.com>
2021-04-09 11:10:06 -0300

    CHanges in text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,44 +1,55 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
 
-import Navbar from '../components/Navbar'
-import Profile from '../components/Profile'
+import Navbar from "../components/Navbar";
+import Profile from "../components/Profile";
 
-import css from '../css/base.module.css'
+import css from "../css/base.module.css";
 
 export default function About() {
-    return (
-        <div className={css.container}>
-            <Head>
-                <title>Sobre mim</title>
-            </Head>
+  return (
+    <div className={css.container}>
+      <Head>
+        <title>Sobre mim</title>
+      </Head>
 
-            <Navbar />
+      <Navbar />
 
-            <main>
-                <Profile />
+      <main>
+        <Profile />
 
-                <section>
-                    <header>
-                        <h2> Sobre mim </h2>
-                        <hr/>
-                    </header>
+        <section>
+          <header>
+            <h2> Sobre mim </h2>
+            <hr />
+          </header>
 
-                    <article>
-                        <p> Olá. Meu nome é Marcelino Teixeira, eu tenho 18 anos e estudo no <a>Instituto Federal do Rio Grande do Sul - 
-                            Campus Osório</a>, onde faço o curso Técnico em Informática Integrado ao Ensino Médio. No ano de 2020, fui 
-                            introduzido à matéria de programação web, pela qual me interessei e comecei a estudar mais por conta própria. 
-                            Fiz cursos à distância, além de participar de eventos como a Next Level Week, da Rocketseat, o que me proporcionou 
-                            bastante prática e aprendizado. Atualmente, domino <a><i>HTML</i></a> e <a><i>CSS</i></a>, e estou focado em 
-                            praticar a biblioteca JavaScript <a><i>React</i></a> juntamente do framework <a><i>Next.js</i></a>. </p>
+          <article>
+            <p>
+              Olá. Meu nome é Marcelino Teixeira, eu tenho 18 anos e estudo 
+              no <strong> Instituto Federal do Rio Grande do Sul - Campus Osório </strong>, 
+              onde faço o curso <strong> Técnico em Informática</strong> Integrado ao Ensino 
+              Médio. No ano de 2020, fui introduzido à matéria de programação
+              web, pela qual me interessei e comecei a estudar mais por conta
+              própria. Fiz cursos à distância, além de participar de eventos
+              como a Next Level Week, da Rocketseat, o que me proporcionou
+              bastante prática e aprendizado. Atualmente, 
+              domino <strong><i> HTML </i></strong> e <strong><i> CSS </i></strong>, 
+              e estou focado em praticar a biblioteca 
+              JavaScript <strong><i>React</i></strong> juntamente 
+              do framework <strong><i>Next.js</i></strong>.
+            </p>
 
-                        <p> Este site foi inteiramente desenvolvido por mim em React, utilizando as 
-                            ferramentas do Next.js. No meu <a>Portfólio</a>, 
-                            é possível encontrar tanto projetos pessoais (que por enquanto ainda são poucos) 
-                            quanto os da NLW que participei, por exemplo, além de trabalhos do meu curso técnico. </p>
-                    </article>
-                </section>
-            </main>
-        </div>
-    )
+            <p>
+              Este site foi inteiramente desenvolvido por mim em React,
+              utilizando as ferramentas do Next.js. Em meu <strong>Portfólio</strong>, 
+              é possível encontrar tanto projetos pessoais (que por enquanto ainda são 
+              poucos) quanto os da NLW que participei, por exemplo, além de trabalhos 
+              do meu curso técnico.
+            </p>
+          </article>
+        </section>
+      </main>
+    </div>
+  );
 }
