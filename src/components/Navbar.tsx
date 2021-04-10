202c18fa026ea9406857f9117bcf1727504dce98
Marcelino <marceometry@gmail.com>
2021-04-10 02:45:04 -0300

    navbar routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import css from '../css/components/navbar.module.css'
 
 export default function Navbar() {
+    const router = useRouter()
+
+    function isActive(route: string) {
+        if (route == router.pathname) {
+            return css.active
+        } else ""
+    }
+
     return (
         <div className={css.container}>
             <header>
@@ -9,9 +18,12 @@ export default function Navbar() {
             </header>
 
             <ul id="navbar">
-                <Link href="/about"><a><li className={css.active}> Sobre mim </li></a></Link>
-                <Link href="/portfolio"><a><li> Portfólio </li></a></Link>
-                <Link href="/contact"><a><li> Contato </li></a></Link>
+                <Link href="/about"><a><li className={isActive('/about')}> Sobre mim </li></a></Link>
+
+                <Link href="/portfolio"><a><li className={isActive('/portfolio')}> Portfólio </li></a></Link>
+
+                <Link href="/contact"><a><li className={isActive('/contact')}> Contato </li></a></Link>
+
                 <Link href="https://github.com/marceometry">
                     <a target="_blank" rel="external noopener noreferrer nofollow">
                         <li> GitHub </li>
